c29194030da0d540b7c4ad2bfcb69fd462f3b9d9
znicholas913 <156281035+znicholas913@users.noreply.github.com>
2024-02-18 23:56:50 -0700

    tests

--- SpreadsheetTests/SpreadsheetTests.cs
+++ SpreadsheetTests/SpreadsheetTests.cs
@@ -81,77 +81,77 @@ public class SpreadsheetTests
         tester.Save("testSave.txt");
         Assert.AreEqual("Default", tester.GetSavedVersion("testSave.txt"));
     }
-//     /// <summary>
-//     /// This tests to see if it correctly picks out the cells that have something in them.
-//     /// </summary>
-//     [TestMethod]
-//     public void TestGetNamesOfNonEmptyCellContents()
-//     {
-//         tester.SetCellContents("A1", "");
-//         tester.SetCellContents("A2", "");
-//         tester.SetCellContents("A3", "");
-//         tester.SetCellContents("A4", "Ants");
-//         tester.SetCellContents("A5", 5);
-//         Assert.AreEqual("A4",tester.GetNamesOfAllNonemptyCells().ToList()[0]);
-//         Assert.AreEqual("A5",tester.GetNamesOfAllNonemptyCells().ToList()[1]);
-//     }
-// /// <summary>
-// ///Tests to see if get cell contents works with all 3 of the types accepted.
-// /// </summary>
-//     [TestMethod]
-//     public void TestGetCellContents()
-//     {
-//         Formula testing = new Formula("A1 + 5");
-//         tester.SetCellContents("A1", 5);
-//         tester.SetCellContents("A2", testing);
-//         tester.SetCellContents("A3", "hi");
-//         Assert.AreEqual("hi",tester.GetCellContents("A3"));
-//         Assert.AreEqual(testing,tester.GetCellContents("A2"));
-//         Assert.AreEqual(5.0 ,tester.GetCellContents("A1"));
-//     }
-// /// <summary>
-// /// Checks to make sure getcellcontents return an empty string if the cells don't have anything in it.
-// /// </summary>
-//     [TestMethod]
-//     public void TestGetCellContentsWithInvalidName()
-//     {
-//         tester.SetCellContents("A1", "");
-//         tester.SetCellContents("A2", "");
-//         tester.SetCellContents("A3", "");
-//         tester.SetCellContents("A4", "Ants");
-//         tester.SetCellContents("A5", 5);
-//         Assert.AreEqual("", tester.GetCellContents("A6"));
-//     }
-//
-//     [TestMethod]
-//     [ExpectedException(typeof(CircularException))]
-//     public void TestCircularException()
-//     {
-//         Formula test1 = new Formula("B1");
-//         Formula test2 = new Formula("A1*A1");
-//         Formula test3 = new Formula("B1+A1");
-//         Formula test4 = new Formula("B1-C1");
-//         tester.SetCellContents("A1", test1);
-//         tester.SetCellContents("B1", test2);
-//         tester.SetCellContents("C1", test3);
-//         tester.SetCellContents("D1", test4);
-//     }
-//
-//     // [TestMethod]
-//     // public void TestGetDirectDependents()
-//     // {
-//     //     Formula test1 = new Formula("3");
-//     //     Formula test2 = new Formula("A1*A1");
-//     //     Formula test3 = new Formula("B1+A1");
-//     //     Formula test4 = new Formula("B1-C1");
-//     //     tester.SetCellContents("A1", test1);
-//     //     tester.SetCellContents("B1", test2);
-//     //     tester.SetCellContents("C1", test3);
-//     //     tester.SetCellContents("D1", test4);
-//     //     Assert.AreEqual(2,tester.forTestsOnly("A1").Count);
-//     //     Assert.AreEqual("B1",tester.forTestsOnly("A1")[0]);
-//     //     Assert.AreEqual("C1",tester.forTestsOnly("A1")[1]);
-//     // }
+    /// <summary>
+    /// This tests to see if it correctly picks out the cells that have something in them.
+    /// </summary>
+    [TestMethod]
+    public void TestGetNamesOfNonEmptyCellContents()
+    {
+        tester.SetContentsOfCell("A1", "");
+        tester.SetContentsOfCell("A2", "");
+        tester.SetContentsOfCell("A3", "");
+        tester.SetContentsOfCell("A4", "Ants");
+        tester.SetContentsOfCell("A5", "5");
+        Assert.AreEqual("A4",tester.GetNamesOfAllNonemptyCells().ToList()[0]);
+        Assert.AreEqual("A5",tester.GetNamesOfAllNonemptyCells().ToList()[1]);
+    }
+/// <summary>
+///Tests to see if get cell contents works with all 3 of the types accepted.
+/// </summary>
+    [TestMethod]
+    public void TestGetCellContents()
+    {
+        Formula testing = new Formula("A1 + 5");
+        tester.SetContentsOfCell("A1", "5");
+        tester.SetContentsOfCell("A2", "A1 + 5");
+        tester.SetContentsOfCell("A3", "hi");
+        Assert.AreEqual("hi",tester.GetCellContents("A3"));
+        Assert.AreEqual(testing,tester.GetCellContents("A2"));
+        Assert.AreEqual(5.0 ,tester.GetCellContents("A1"));
+    }
+/// <summary>
+/// Checks to make sure getcellcontents return an empty string if the cells don't have anything in it.
+/// </summary>
+    [TestMethod]
+    public void TestGetCellContentsWithInvalidName()
+    {
+        tester.SetContentsOfCell("A1", "");
+        tester.SetContentsOfCell("A2", "");
+        tester.SetContentsOfCell("A3", "");
+        tester.SetContentsOfCell("A4", "Ants");
+        tester.SetContentsOfCell("A5", "5");
+        Assert.AreEqual("", tester.GetCellContents("A6"));
+    }
+
+    // [TestMethod]
+    // [ExpectedException(typeof(CircularException))]
+    // public void TestCircularException()
+    // {
+    //     Formula test1 = new Formula("B1");
+    //     Formula test2 = new Formula("A1*A1");
+    //     Formula test3 = new Formula("B1+A1");
+    //     Formula test4 = new Formula("B1-C1");
+    //     tester.SetContentsOfCell("A1", test1);
+    //     tester.SetContentsOfCell("B1", test2);
+    //     tester.SetContentsOfCell("C1", test3);
+    //     tester.SetContentsOfCell("D1", test4);
+    // }
+
+    // [TestMethod]
+    // public void TestGetDirectDependents()
+    // {
+    //     Formula test1 = new Formula("3");
+    //     Formula test2 = new Formula("A1*A1");
+    //     Formula test3 = new Formula("B1+A1");
+    //     Formula test4 = new Formula("B1-C1");
+    //     tester.SetCellContents("A1", test1);
+    //     tester.SetCellContents("B1", test2);
+    //     tester.SetCellContents("C1", test3);
+    //     tester.SetCellContents("D1", test4);
+    //     Assert.AreEqual(2,tester.forTestsOnly("A1").Count);
+    //     Assert.AreEqual("B1",tester.forTestsOnly("A1")[0]);
+    //     Assert.AreEqual("C1",tester.forTestsOnly("A1")[1]);
+    // }
 //     [TestMethod(), Timeout( 2000 )]
 //         [TestCategory( "2" )]
 //         [ExpectedException( typeof( InvalidNameException ) )]
